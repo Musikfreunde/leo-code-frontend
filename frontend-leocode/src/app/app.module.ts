c2298a7ea216aa2201a139c52fda969df2b9f149
zbenii <besic.beni@gmx.at>
2021-04-27 13:30:44 +0200

    htl-leonding-project/leo-code#13 added teacher upload

--- frontend-leocode/src/app/app.module.ts
+++ frontend-leocode/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {MaterialModule} from './material-module';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +10,12 @@ import { LoginPageComponent } from './login-page/login-page.component';
 import { TeacherPageComponent } from './teacher-page/teacher-page.component';
 import { StudentPageComponent } from './student-page/student-page.component';
 import { GuestPageComponent } from './guest-page/guest-page.component';
+import { UploadComponent } from './teacher-page/upload/upload.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import { NgxMatFileInputModule } from '@angular-material-components/file-input';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {FormsModule} from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -16,13 +23,20 @@ import { GuestPageComponent } from './guest-page/guest-page.component';
     LoginPageComponent,
     TeacherPageComponent,
     StudentPageComponent,
-    GuestPageComponent
+    GuestPageComponent,
+    UploadComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    NgxMatFileInputModule,
+    FlexLayoutModule,
+    FormsModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend-leocode/src/app/teacher-page/teacher-page.component.ts
+++ frontend-leocode/src/app/teacher-page/teacher-page.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeacherPageComponent implements OnInit {
 
+  isDisplay = true;
+
+  toggleDisplay(): void {
+    this.isDisplay = !this.isDisplay;
+  }
+
   constructor() { }
 
   ngOnInit(): void {
