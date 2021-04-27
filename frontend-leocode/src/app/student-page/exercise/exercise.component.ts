bf53b86d86ea6c3a9ec8b3af299c885297710642
SpasenovicBozidar <32099007+SpasenovicBozidar@users.noreply.github.com>
2021-04-27 15:12:19 +0200

    htl-leonding-project/leo-code#14 added List of Exercises to view

--- frontend-leocode/src/app/student-page/exercise/exercise.component.ts
+++ frontend-leocode/src/app/student-page/exercise/exercise.component.ts
@@ -5,11 +5,25 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './exercise.component.html',
   styleUrls: ['./exercise.component.scss']
 })
+
 export class ExerciseComponent implements OnInit {
+  exercisesList: Exercise[] = [new Exercise('Example 01: Java for beginners', 'Benjo', 'Simple Java program'),
+    new Exercise('Example 02: Java Calculator', 'Boki', 'Calculator'),
+    new Exercise('Example 03: Java CoffeeSlotMachine', 'David', 'CoffeeSlotMachine')
+  ];
+  nameList: string[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.nameList = this.exercisesList.map(e => e.name);
   }
 
+  findExercise(name: string): Exercise{
+    return this.exercisesList.find(e => e.name === name) as Exercise;
+  }
+}
+export class Exercise{
+  constructor(public name: string, public author: string, public description: string) {
+  }
 }
