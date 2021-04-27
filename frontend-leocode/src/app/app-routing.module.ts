a7dd00a3621e73a604ec61c99f33f2573592eb34
zbenii <besic.beni@gmx.at>
2021-04-27 12:53:37 +0200

    htl-leonding-project/leo-code#13 added login component

--- frontend-leocode/src/app/app-routing.module.ts
+++ frontend-leocode/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginPageComponent} from './login-page/login-page.component';
+import {StudentPageComponent} from './student-page/student-page.component';
+import {TeacherPageComponent} from './teacher-page/teacher-page.component';
+import {GuestPageComponent} from './guest-page/guest-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'login'},
+  {path: 'login', component: LoginPageComponent},
+  {path: 'student', component: StudentPageComponent},
+  {path: 'teacher', component: TeacherPageComponent},
+  {path: 'guest', component: GuestPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend-leocode/src/app/app.module.ts
+++ frontend-leocode/src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MaterialModule} from './material-module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { TeacherPageComponent } from './teacher-page/teacher-page.component';
+import { StudentPageComponent } from './student-page/student-page.component';
+import { GuestPageComponent } from './guest-page/guest-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    TeacherPageComponent,
+    StudentPageComponent,
+    GuestPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
