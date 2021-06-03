31e84b0453c5a36edaac56d366ad30bfc7222d3a
zbenii <besic.beni@gmx.at>
2021-06-03 11:22:09 +0200

    added new frontend with combined with donnabauer frontend

---
+++ frontend/src/app/student-page/student-page.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import {Observable} from "rxjs";
+import {BreakpointObserver, Breakpoints} from "@angular/cdk/layout";
+import {map, shareReplay} from "rxjs/operators";
+
+@Component({
+  selector: 'app-student-page',
+  templateUrl: './student-page.component.html',
+  styleUrls: ['./student-page.component.css']
+})
+export class StudentPageComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+}
