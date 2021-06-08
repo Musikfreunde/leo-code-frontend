8326d9579f94c408e5a3c69ba679f01b4f5dc387
zbenii <besic.beni@gmx.at>
2021-06-08 10:05:21 +0200

    change

--- frontend/src/app/services/http.service.ts
+++ frontend/src/app/services/http.service.ts
@@ -1,5 +1,5 @@
 import {Injectable, NgZone} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Example} from '../model/example.model';
 import {Submission} from '../model/submission.model';
@@ -9,28 +9,35 @@ import {Submission} from '../model/submission.model';
 })
 export class HttpService {
 
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Access-Control-Allow-Origin':  '*',
+    })
+  };
+
   BASE_URL = 'http://localhost:9090/';
 
   constructor(private http: HttpClient,
               private _zone: NgZone) { }
 
   getExampleList(): Observable<Example[]> {
-    return this.http.get<Example[]>(this.BASE_URL + 'example/list');
+    return this.http.get<Example[]>(this.BASE_URL + 'example/list', this.httpOptions);
   }
 
   getExampleById(id: number): Observable<Example>{
-    return this.http.get<Example>(this.BASE_URL  + 'example/' + id);
+    return this.http.get<Example>(this.BASE_URL  + 'example/' + id, this.httpOptions);
   }
 
   createExample(form: HTMLFormElement): Observable<Example>{
-    return this.http.post<Example>(this.BASE_URL + 'example', new FormData(form));
+    return this.http.post<Example>(this.BASE_URL + 'example', new FormData(form), this.httpOptions);
   }
 
   testExample(form: FormData): Observable<any>{
-    return this.http.post<any>(this.BASE_URL + 'submission', form);
+    return this.http.post<any>(this.BASE_URL + 'submission', form, this.httpOptions);
   }
 
   getSubmissionStatusSse(id: number): Observable<MessageEvent> {
+
     return Observable.create(observer => {
       const eventSource = new EventSource(this.BASE_URL + 'submission/' + id);
       eventSource.onmessage = event => {
@@ -47,6 +54,6 @@ export class HttpService {
   }
 
   getFinishedSubmissions(username: string): Observable<Submission[]> {
-    return this.http.get<Submission[]>(this.BASE_URL + 'submission/history/' + username);
+    return this.http.get<Submission[]>(this.BASE_URL + 'submission/history/' + username, this.httpOptions);
   }
 }
